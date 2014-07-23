4a73c7f76841271f24ee7f4accaadc8371d39ba8
ettore90 <besartshyti@gmail.com>
2014-07-23 14:33:04 +0000

    better code

--- _site/assets/js/twitter.js
+++ _site/assets/js/twitter.js
@@ -1,11 +1,12 @@
-    $.ajax({
-        url: "http://obscure-ridge-8042.herokuapp.com/",
-        dataType: "json",
+$.ajax({
+  url: "http://obscure-ridge-8042.herokuapp.com/",
+  dataType: "json",
 
-        success: function(data) {
-          $.each(data.statuses, function () {
-            $( "#tweets" ).append('<div style="float:left">' 
-                                  + '<img src="' + this['img'] + '" style="max-height:200px;padding:10px">' 
-                                  + '</div>');
-          });
-       }});
+  success: function(data) {
+    $.each(data.statuses, function () {
+      $( "#tweets" ).append('<div style="float:left">' 
+                            + '<img src="' + this['img'] + '" style="max-height:200px;padding:10px">' 
+                            + '</div>');
+    });
+  }
+});
